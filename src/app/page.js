7b5b7fe3ae21386fd28574cd231ff823f6f7a61a
dalex1981a <168557868+dalex1981a@users.noreply.github.com>
2024-07-24 22:44:59 +0000

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
+"use client"
+
 import Image from "next/image";
+import { TodoList } from "@/Components/TodoList";
 
 export default function Home() {
   return (
-    <div className='container mx-auto text-xl'>Hello MDC27</div>
+    
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">       
+      < TodoList user="user2" />
+      </div>
   );
 }
